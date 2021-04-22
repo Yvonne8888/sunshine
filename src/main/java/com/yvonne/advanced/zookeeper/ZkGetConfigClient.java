7281ebb40daf376d4adeb967d6054a88f23bf0a1
Yvonne8888 <826726727@qq.com>
2021-04-22 23:23:52 +0800

    git start on 04/21/2021

--- src/main/java/com/yvonne/advanced/zookeeper/ZkGetConfigClient.java
+++ src/main/java/com/yvonne/advanced/zookeeper/ZkGetConfigClient.java
@@ -42,7 +42,6 @@ public class ZkGetConfigClient {
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         }
